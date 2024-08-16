44af2c1fdd324aef171b7b223321c996824432ef
Dragos Andrei <hi.dragos.andrei@gmail.com>
2024-08-16 02:49:42 +0200

    Initial commit

---
+++ src/app/cv/cv.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cv',
+  standalone: true,
+  imports: [],
+  templateUrl: './cv.component.html',
+  styleUrl: './cv.component.scss'
+})
+export class CvComponent {
+
+}
