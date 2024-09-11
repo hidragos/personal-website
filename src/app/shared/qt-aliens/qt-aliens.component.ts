a3fcbb1d0abe274611cd91a6c00da361509037ae
Dragos Andrei <hi.dragos.andrei@gmail.com>
2024-09-11 17:17:03 +0200

    Quite possible is that even more pzaz was added

--- src/app/shared/qt-aliens/qt-aliens.component.ts
+++ src/app/shared/qt-aliens/qt-aliens.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatRippleModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-qt-aliens',
   standalone: true,
-  imports: [],
+  imports: [MatRippleModule],
   templateUrl: './qt-aliens.component.html',
   styleUrl: './qt-aliens.component.scss',
 })
