6e1685bc114465ad27c960dee65cdf3027a24e8c
hidragos <hidragos@users.noreply.github.com>
2024-09-11 15:59:40 +0000

    deploy: 261bbaff7e482356db443e5e6b3234b659f827da

---
+++ chunk-6VSABH3K.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-OXGIR2WM.js";import"./chunk-KDHYEXT3.js";export{a as HeartLoveComponent};
--- chunk-C7DOC7O2.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./chunk-KX7QNGOF.js";import"./chunk-34V5KNHM.js";import"./chunk-SGYY3ZVI.js";export{a as ResumeComponent};
