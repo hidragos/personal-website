85b6619abcb8f57056c29f1825c18a205c988de7
Dragos Andrei <hi.dragos.andrei@gmail.com>
2024-09-13 09:25:35 +0200

    Add pzaz

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import { TranslocoDirective } from '@jsverse/transloco';
+import { version } from 'package.json';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [TranslocoDirective],
+  imports: [TranslocoDirective, MatButton],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  version = version;
+}
