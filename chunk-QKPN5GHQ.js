fc62e4c00caffa419d1327f15e449628ddf403e7
hidragos <hidragos@users.noreply.github.com>
2024-09-21 14:27:22 +0000

    deploy: f2c6868cce6021569bcbd5c82e8078cd2dc0bd9c

--- chunk-QKPN5GHQ.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-3H34234B.js";import"./chunk-DNSHJFD5.js";import"./chunk-WXI33M2S.js";import"./chunk-IIQSA42B.js";import"./chunk-YYXHG7V6.js";import"./chunk-5BPRXVO3.js";import"./chunk-ODN5LVDJ.js";export{a as ResumeComponent};
